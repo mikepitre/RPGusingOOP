702a79802098c30f3e04af168f13d44ed0f08979
mikepitre <michaelepitre@gmail.com>
2015-10-22 14:58:34 -0500

    created Character class

---
+++ rpgoop/Character.swift
@@ -0,0 +1,46 @@
+//
+//  Character.swift
+//  rpgoop
+//
+//  Created by Mike Pitre on 10/22/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(hp: Int, attackPwr: Int) {
+        self._hp = hp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        return true
+    }
+}
