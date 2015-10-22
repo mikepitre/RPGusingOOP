3eb71fe4415c0ecdd305ebc82dd4cd6c29ca275f
mikepitre <michaelepitre@gmail.com>
2015-10-22 18:11:37 -0500

    added some outlets, changed chest from image to button

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
 
 }
